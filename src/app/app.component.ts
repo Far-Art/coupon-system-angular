437ca17a29647157cc0cddd8986f7d2ca15529ef
far-art <arturfarmanov91@gmail.com>
2024-07-12 15:55:03 +0300

    further development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {AuthService} from './auth/auth.service';
 import {ModalService} from './shared/components/modal/modal.service';
-import {ToastService} from './core/toasts/toast.service';
 
 
 @Component({
@@ -14,7 +13,7 @@ export class AppComponent implements OnInit {
   static title = 'coupon system angular';
   @ViewChild('content', {static: true}) content: ElementRef<HTMLElement>
 
-  constructor(private authService: AuthService, private modalService: ModalService, private renderer: Renderer2, public toastService: ToastService) {}
+  constructor(private authService: AuthService, private modalService: ModalService, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.authService.autoLogin();
@@ -24,16 +23,6 @@ export class AppComponent implements OnInit {
       } else {
         this.renderer.removeAttribute(this.content.nativeElement, 'inert');
       }
-    });
-    this.click(); // TODO remove
-  }
-
-  // TODO remove
-  click() {
-    this.toastService.notify({
-      message: 'created new toast',
-      header: 'header',
-      style: 'warning'
     });
   }
 }
