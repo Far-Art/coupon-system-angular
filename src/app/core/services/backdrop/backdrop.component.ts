e03395ff6094a08e1aae559a1442e0adec7d80a0
far-art <arturfarmanov91@gmail.com>
2024-07-21 20:52:36 +0300

    added select component

--- src/app/core/services/backdrop/backdrop.component.ts
+++ src/app/core/services/backdrop/backdrop.component.ts
@@ -1,5 +1,4 @@
-import {Component, ElementRef, HostListener, OnDestroy, OnInit, Self} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {Component, ElementRef, HostListener, Self} from '@angular/core';
 import {BackdropService} from './backdrop.service';
 
 
@@ -8,29 +7,15 @@ import {BackdropService} from './backdrop.service';
   templateUrl: './backdrop.component.html',
   styleUrls: ['./backdrop.component.scss']
 })
-export class BackdropComponent implements OnInit, OnDestroy {
-
-  isVisible: boolean;
-
-  private subscription: Subscription;
+export class BackdropComponent {
 
   constructor(private backdropService: BackdropService, @Self() elRef: ElementRef) {
     backdropService['registerContainer'](elRef);
   }
 
-  ngOnInit(): void {
-    this.subscription = this.backdropService.isVisible$().subscribe(isVisible => {
-      this.isVisible = isVisible;
-    });
-  }
-
   @HostListener('click', ['$event'])
   onClick(event: MouseEvent): void {
     this.backdropService['onBackdropClick'](event);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 
 }
